10c395d5de50ddcfbe847497c49863d8433c4d2f
Fletchi <juliusblohm@icloud.com>
2026-08-15 01:14:54 +0200

    add all signals for sidebar buttons

---
+++ scripts/ui_1.gd
@@ -0,0 +1,36 @@
+extends Panel
+
+@onready var main_panel: MarginContainer = $RootLayout/MainPanel
+@onready var mods_page: Panel = $ModsPage
+
+
+func _on_btn_instances_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_btn_mods_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_btn_resourcepacks_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_btn_shader_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_btn_worlds_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_btn_screenshots_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_btn_you_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_btn_settings_pressed() -> void:
+	pass # Replace with function body.
